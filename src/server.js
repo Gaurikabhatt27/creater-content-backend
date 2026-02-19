6a4ce1f05a1f9527b942cc87b6aa9e68bdf42360
Gaurika Bhatt <gaurikabhatt27@gmail.com>
2026-02-20 02:19:16 +0530

    feat: set up MVC folder structure

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5500;
+
+app.listen(PORT , () => {
+    console.log(`Server Running on port ${PORT}`);
+})
