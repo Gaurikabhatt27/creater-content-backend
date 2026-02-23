a169dd08c40c94729f9d31a4b5483525963be927
Gaurika Bhatt <gaurikabhatt27@gmail.com>
2026-02-23 11:20:08 +0530

    feat: Asset feature logic added

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,17 @@
 import express from "express";
-import { signup, login ,logout ,sendOtpController ,verifyOtpController} from "../controllers/authController.js";
+import { signup, login ,logout ,sendOtpController ,verifyOtpController, getMe} from "../controllers/authController.js";
 import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/me", protect, (req, res) => {
-  res.json(req.user);
-});
+// router.get("/me", protect, (req, res) => {
+//   res.json(req.user);
+// });
 router.post("/logout", logout);
 router.post("/send-otp", sendOtpController);
 router.post("/verify-otp", verifyOtpController);
+router.get("/me", getMe, protect); 
 
 export default router;
